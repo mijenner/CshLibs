4d0726037ec2a17e915c285dadde917a7d231b59
Michael Jenner <mijenner@gmail.com>
2021-10-31 12:19:12 +0100

    Added ToolsLibrary (.net core)

---
+++ CshLibs/TestToolsLibrary/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.IO;
+using ToolsLibrary;
+
+namespace TestToolsLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (OperatingSystemInfo.IsWindows())
+            {
+                Console.WriteLine("Platform: Windows");
+            }
+            else if (OperatingSystemInfo.IsMacOS())
+            {
+                Console.WriteLine("Platform: MacOS");
+            }
+            else if (OperatingSystemInfo.IsLinux())
+            {
+                Console.WriteLine("Platform: Linux");
+            }
+
+
+            string folder = "";
+            if (FolderSettings.GetFolderApp(ref folder))
+            {
+                Console.WriteLine("GetFolderApp: " + folder);
+            }
+
+
+            if (FolderSettings.GetFolderProgramData(ref folder))
+            {
+                Console.WriteLine("GetFolderProgramData: " + folder);
+            }
+
+            if (FolderSettings.GetFolderAppdataRoaming(ref folder))
+            {
+                Console.WriteLine("GetFolderAppdataRoaming: " + folder);
+            }
+
+            if (FolderSettings.GetFolderAppdataLocal(ref folder))
+            {
+                Console.WriteLine("GetFolderAppdataLocal: " + folder);
+            }
+
+            if (FolderSettings.GetFolderDocuments(ref folder))
+            {
+                Console.WriteLine("GetFolderDocuments: " + folder);
+            }
+            string myDocs = folder;
+
+            if (FolderSettings.GetFolderDesktop(ref folder))
+            {
+                Console.WriteLine("GetFolderDesktop: " + folder);
+            }
+
+            if (FileFolder.IsDirectoryWritable(myDocs, false))
+            {
+                Console.WriteLine("Folder " + myDocs + " is writable");
+            }
+            else
+            {
+                Console.WriteLine("Folder " + myDocs + " is NOT writable");
+            }
+
+            string myDocs2 = myDocs + "ABC";
+            if (FileFolder.IsDirectoryWritable(myDocs2, false))
+            {
+                Console.WriteLine("Folder " + myDocs2 + " is writable");
+            }
+            else
+            {
+                Console.WriteLine("Folder " + myDocs2 + " is NOT writable");
+            }
+
+
+            // Save application settings to AppData/Roaming 
+            AppSettings appSettings = new AppSettings();
+            Console.WriteLine("Preparing to store application settings");
+            if (FolderSettings.GetFolderAppdataRoaming(ref folder))
+            {
+                Console.WriteLine("Got path to AppData\\Roaming = " + folder);
+            }
+            else
+            {
+                Console.WriteLine("Problem getting AppdataRoaming path");
+                return;
+            }
+            string serFile = Path.Combine(folder, "AppSettings.xml");
+            Console.WriteLine("Saves settings to " + serFile);
+            ObjToXml.WriteXML<AppSettings>(appSettings, serFile);
+            AppSettings appSettings2 = new AppSettings();
+            ObjToXml.ReadXML<AppSettings>(ref appSettings2, serFile);
+            Console.WriteLine("After reading settings we get: ");
+            Console.WriteLine(appSettings2.Heltal);
+            Console.WriteLine(appSettings2.KommatalFloat);
+            Console.WriteLine(appSettings2.KommatalDouble);
+            Console.WriteLine(appSettings2.KommatalDecimal);
+            Console.WriteLine(appSettings2.Tegn);
+            Console.WriteLine(appSettings2.Streng);
+
+
+            // Assembly info: 
+            About anAbout;
+
+            Assy.GetAssy<Program>(out anAbout);
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Assembly info: ");
+            Console.WriteLine("Title = " + anAbout.Title);
+            Console.WriteLine("Company = " + anAbout.Company);
+            Console.WriteLine("Version = " + anAbout.Version);
+            Console.WriteLine("FileVersion = " + anAbout.FileVersion);
+
+            Assy.GetAssy<About>(out anAbout);
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Assembly info: ");
+            Console.WriteLine("Title = " + anAbout.Title);
+            Console.WriteLine("Company = " + anAbout.Company);
+            Console.WriteLine("Version = " + anAbout.Version);
+            Console.WriteLine("FileVersion = " + anAbout.FileVersion);
+
+            Console.ReadKey();
+
+
+        }
+    }
+
+    public class AppSettings
+    {
+        public int Heltal { get; set; }
+        public float KommatalFloat { get; set; }
+        public double KommatalDouble { get; set; }
+        public decimal KommatalDecimal { get; set; }
+        public char Tegn { get; set; }
+        public string Streng { get; set; }
+
+        public AppSettings()
+        {
+            Heltal = 2;
+            KommatalFloat = 3.1425f;
+            KommatalDouble = 2.78;
+            KommatalDecimal = 0.10m;
+            Tegn = 'p';
+            Streng = @"Hello, \world!";
+        }
+
+    }
+}
---
+++ CshLibs/ToolsLibrary/ToolsLibrary.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Xml.Serialization;
+
+namespace ToolsLibrary
+{
+    public static class OperatingSystemInfo
+    {
+        // Usage:
+        //   if(OperatingSystem.IsWindows()) ... 
+        //   if(OperatingSystem.IsMacOS()) ... 
+        public static bool IsWindows() =>
+            RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+        public static bool IsMacOS() =>
+            RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+
+        public static bool IsLinux() =>
+            RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+    }
+
+
+    public static class FolderSettings
+    {
+
+        public static bool GetFolderApp(ref string path)
+        {
+            try
+            {
+                path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                if (!string.IsNullOrWhiteSpace(path))
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                return false;
+                // throw;
+            }
+        }
+
+
+        public static bool GetFolderProgramData(ref string path)
+        {
+            try
+            {
+                path = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+                if (!string.IsNullOrWhiteSpace(path))
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                return false;
+                // throw;
+            }
+
+        }
+
+
+        public static bool GetFolderAppdataLocal(ref string path)
+        {
+            try
+            {
+                path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                if (!string.IsNullOrWhiteSpace(path))
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                return false;
+                // throw;
+            }
+
+        }
+
+        public static bool GetFolderAppdataRoaming(ref string path)
+        {
+            try
+            {
+                path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                if (!string.IsNullOrWhiteSpace(path))
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                return false;
+                // throw;
+            }
+
+        }
+
+        public static bool GetFolderDesktop(ref string path)
+        {
+            try
+            {
+                path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                if (!string.IsNullOrWhiteSpace(path))
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                return false;
+                // throw;
+            }
+
+        }
+
+        public static bool GetFolderDocuments(ref string path)
+        {
+            try
+            {
+                path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                if (!string.IsNullOrWhiteSpace(path))
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                return false;
+                // throw;
+            }
+
+        }
+
+    }
+
+    public static class FileFolder
+    {
+
+        public static bool IsDirectoryWritable(string dirPath, bool throwIfFails = false)
+        {
+            try
+            {
+                using (FileStream fs = File.Create(Path.Combine(dirPath, Path.GetRandomFileName()), 1, FileOptions.DeleteOnClose))
+                { }
+                return true;
+            }
+            catch
+            {
+                if (throwIfFails)
+                    throw;
+                else
+                    return false;
+            }
+        }
+    }
+
+    public static class ObjToXml
+    {
+        public static void WriteXML<T>(T anObject, string fileNameWithPath)
+        {
+
+            // Note: Object, must have a constructor without parameters for
+            // below to work AND only properties (public) are saved! 
+            XmlSerializer writer = new XmlSerializer(typeof(T));
+            FileStream file = File.Create(fileNameWithPath);
+            writer.Serialize(file, anObject);
+            file.Close();
+        }
+
+        public static void ReadXML<T>(ref T anObject, string fileNameWithPath)
+        {
+            XmlSerializer reader = new XmlSerializer(typeof(T));
+            StreamReader file = new StreamReader(fileNameWithPath);
+            anObject = (T)reader.Deserialize(file);
+            file.Close();
+
+        }
+    }
+
+    public struct About
+    {
+        public string Title;
+        public string Company;
+        public string Version;
+        public string FileVersion;
+    }
+
+    public static class Assy
+    {
+
+        public static bool GetAssy<T>(out About anAbout)
+        {
+            Assembly anAssy = typeof(T).Assembly;
+
+            object[] attribs;
+            attribs = anAssy.GetCustomAttributes(typeof(AssemblyTitleAttribute), true);
+            if (attribs.Length > 0)
+            {
+                anAbout.Title = ((AssemblyTitleAttribute)attribs[0]).Title;
+            }
+            else
+            {
+                anAbout.Title = "";
+            }
+
+            attribs = anAssy.GetCustomAttributes(typeof(AssemblyCompanyAttribute), true);
+            if (attribs.Length > 0)
+            {
+                anAbout.Company = ((AssemblyCompanyAttribute)attribs[0]).Company;
+            }
+            else
+            {
+                anAbout.Company = "";
+            }
+
+            attribs = anAssy.GetCustomAttributes(typeof(AssemblyVersionAttribute), true);
+            if (attribs.Length > 0)
+            {
+                anAbout.Version = ((AssemblyVersionAttribute)attribs[0]).Version;
+            }
+            else
+            {
+                anAbout.Version = "";
+            }
+
+            attribs = anAssy.GetCustomAttributes(typeof(AssemblyFileVersionAttribute), true);
+            if (attribs.Length > 0)
+            {
+                anAbout.FileVersion = ((AssemblyFileVersionAttribute)attribs[0]).Version;
+            }
+            else
+            {
+                anAbout.FileVersion = "";
+            }
+
+
+            return true;
+        }
+    }
+}
